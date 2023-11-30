dc0355bc928e50fbcf5255b77d1652f87b06e3ea
RayenENSEA <129695143+RayenENSEA@users.noreply.github.com>
2023-11-30 09:26:18 +0000

    new question 2

--- question2.c
+++ question2.c
@@ -2,15 +2,20 @@
 #include <string.h>
 #include <stdio.h>
 
+# define BUFFER_SIZE 256
+
 int main() {
 	
     char *welcomeMessage = "Bienvenue dans le Shell ENSEA.\n Pour quitter, tapez 'exit'.\n";
     
     char *prompt = "enseash %";
     
-    char buffer[256];
+    char buffer[BUFFER_SIZE];
+    
+    write(STDOUT_FILENO, welcomeMessage, strlen(welcomeMessage));
     
     while(1){
+		
 		write(STDOUT_FILENO, prompt, strlen(prompt));
 		
 		read(STDIN_FILENO, buffer, sizeof(buffer));
