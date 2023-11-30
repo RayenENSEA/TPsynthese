319242ca0037a6f6e7256ce8211d2c1713cc0a2a
RayenENSEA <129695143+RayenENSEA@users.noreply.github.com>
2023-11-30 09:00:38 +0000

    new question 2

--- question2.c
+++ question2.c
@@ -2,22 +2,25 @@
 #include <string.h>
 #include <stdio.h>
 
-# define BUFFER_SIZE = 256
-
 int main() {
 	
     char *welcomeMessage = "Bienvenue dans le Shell ENSEA.\n Pour quitter, tapez 'exit'.\n";
     
     char *prompt = "enseash %";
     
-    char buffer[BUFFER_SIZE];
+    char buffer[256];
     
     while(1){
 		write(STDOUT_FILENO, prompt, strlen(prompt));
 		
-		read(STDIN_FILENO, buffer, BUFFER_SIZE);
+		read(STDIN_FILENO, buffer, sizeof(buffer));
 		
+		if(strncmp(buffer, "ls", 2) == 0){
+			execl("/bin/ls", buffer, NULL);
+		}
 		
+			
+	}
 
     return 0;
 }
