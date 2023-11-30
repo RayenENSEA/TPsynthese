82cbbf75f4020a11322ef60ffdd3c8a614f0f9ae
lotfihenni <103079040+lotfihenni@users.noreply.github.com>
2023-11-30 11:19:09 +0100

    removing while

--- question2.c
+++ question2.c
@@ -8,37 +8,31 @@
 
 int main() {
 	
-    char *welcomeMessage = "Bienvenue dans le Shell ENSEA.\n Pour quitter, tapez 'exit'.\n";
-    char *prompt = "enseash %";
-    char *error = "Command not found\n";
-    char buffer[BUFFER_SIZE];
-    
-    int command_len;
-    int status;
-    
-    pid_t pid;
-    
-    write(STDOUT_FILENO, welcomeMessage, strlen(welcomeMessage));
-    
-    pid=fork();
-    
-
-		
+	char *welcomeMessage = "Bienvenue dans le Shell ENSEA.\n Pour quitter, tapez 'exit'.\n";
+	char *prompt = "enseash %";
+	char *error = "Command not found\n";
+	char buffer[BUFFER_SIZE];
+	    
+	int command_len;
+	int status;
+	    
+	pid_t pid;
+	    
+	write(STDOUT_FILENO, welcomeMessage, strlen(welcomeMessage));
+	    
+	pid=fork();
+			
+	if(pid==0){
 		write(STDOUT_FILENO, prompt, strlen(prompt));
+		command_len=read(STDIN_FILENO, buffer, sizeof(buffer));
+		buffer[command_len-1]='\0';
 		
-		if(pid==0){
-			command_len=read(STDIN_FILENO, buffer, sizeof(buffer));
-			buffer[command_len-1]='\0';
-			
-			execlp(buffer, buffer, NULL);
-			}
-		
-		else{
-			wait(&status);
-			write(STDOUT_FILENO, error, strlen(error));
-		}
-			
+		execlp(buffer, buffer, NULL);
+		perror("Command not found\n");
+	}
 
-
-    return 0;
+	else{
+		wait(&status);
+	}
+	return 0;
 }
