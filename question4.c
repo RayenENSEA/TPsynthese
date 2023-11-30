4d9895240d73b8246248f1e08438b4b34bb3606b
lotfihenni <103079040+lotfihenni@users.noreply.github.com>
2023-11-30 12:07:17 +0100

    Add files via upload

---
+++ question4.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+
+# define BUFFER_SIZE 256
+
+int main() {
+	
+	char *welcomeMessage = "Bienvenue dans le Shell ENSEA.\n Pour quitter, tapez 'exit'.\n";
+	char *prompt = "enseash %";
+	char *error = "Command not found\n";
+	char *bye = "Bye bye ...\n";
+	char buffer[BUFFER_SIZE];
+	    
+	int command_len;
+	int status;
+	    
+	pid_t pid;
+	    
+	write(STDOUT_FILENO, welcomeMessage, strlen(welcomeMessage));
+	    
+	pid=fork();
+			
+	while(1){
+		if(pid==0){
+				write(STDOUT_FILENO, prompt, strlen(prompt));
+				command_len=read(STDIN_FILENO, buffer, sizeof(buffer));
+				buffer[command_len-1]='\0';
+				if(strncmp(buffer,"exit",4)==0){
+					write(STDOUT_FILENO, bye, strlen(bye));
+					exit(0);
+				}
+				else{
+					execlp(buffer, buffer, NULL);
+					write(STDOUT_FILENO, error, strlen(error));
+				}
+			}
+			else{
+				wait(&status);
+				int code_retour = WEXITSTATUS(status);
+				printf("enseash [exit:%d] %\n",code_retour);
+			}
+		}
+	return 0;
+}
