58f5e347308b1177542ea478e4c6d686536d022d
lotfihenni <103079040+lotfihenni@users.noreply.github.com>
2023-12-07 09:26:42 +0100

    waiting for the next command

--- question2.c
+++ question2.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 
 
@@ -11,28 +12,29 @@ int main() {
 	char *welcomeMessage = "Bienvenue dans le Shell ENSEA.\n Pour quitter, tapez 'exit'.\n";
 	char *prompt = "enseash %";
 	char *error = "Command not found\n";
+	char *bye = "Bye bye ...\n";
 	char buffer[BUFFER_SIZE];
 	    
 	int command_len;
 	int status;
 	    
-	pid_t pid;
-	    
 	write(STDOUT_FILENO, welcomeMessage, strlen(welcomeMessage));
-	    
-	pid=fork();
-			
-	if(pid==0){
+
+	while(1){
 		write(STDOUT_FILENO, prompt, strlen(prompt));
 		command_len=read(STDIN_FILENO, buffer, sizeof(buffer));
 		buffer[command_len-1]='\0';
-		
-		execlp(buffer, buffer, NULL);
-		write(STDOUT_FILENO, error, strlen(error));
-	}
+		pid_t pid;
+		pid=fork();
+		if(pid==0){
+			execlp(buffer, buffer, NULL);
+			write(STDOUT_FILENO, error, strlen(error));
+			exit(EXIT_FAILURE);
+		}
 
-	else{
-		wait(&status);
+		else{
+			wait(&status);
+		}
 	}
 	return 0;
 }
