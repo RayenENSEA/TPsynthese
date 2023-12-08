604e1ae18d09b468fe80cfba6bb62919bd5a7ed6
RayenENSEA <129695143+RayenENSEA@users.noreply.github.com>
2023-12-08 16:22:47 +0000

    new question 1 commented

--- question1.c
+++ question1.c
@@ -4,9 +4,9 @@
 
 int main() {
 	
-    char *buffer = "Bienvenue dans le Shell ENSEA.\n Pour quitter, tapez 'exit'.\n enseash %";
+    char *buffer = "Bienvenue dans le Shell ENSEA.\n Pour quitter, tapez 'exit'.\n enseash %\n"; // welcome message
     
-    write(STDOUT_FILENO, buffer, strlen(buffer));
+    write(STDOUT_FILENO, buffer, strlen(buffer)); //  write the content of the buffer to the standard output associated with the console
 
     return 0;
 }
