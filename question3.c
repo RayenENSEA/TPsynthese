f05aed494c76c2539aa2fd9c565d8caa88f32a98
RayenENSEA <129695143+RayenENSEA@users.noreply.github.com>
2023-12-08 16:23:45 +0000

    new question 3 commented

--- question3.c
+++ question3.c
@@ -12,7 +12,7 @@ int main() {
 	char *welcomeMessage = "Bienvenue dans le Shell ENSEA.\n Pour quitter, tapez 'exit'.\n";
 	char *prompt = "enseash %";
 	char *error = "Command not found\n";
-	char *bye = "Bye bye ...\n";
+	char *bye = "Bye bye ...\n"; // bye bye message 
 	char buffer[BUFFER_SIZE];
 	    
 	int command_len;
@@ -25,9 +25,9 @@ int main() {
 		write(STDOUT_FILENO, prompt, strlen(prompt));
 		command_len=read(STDIN_FILENO, buffer, sizeof(buffer));
 		buffer[command_len-1]='\0';
-		if(strncmp(buffer,"exit",4)==0){
-			write(STDOUT_FILENO, bye, strlen(bye));
-			exit(EXIT_SUCCESS);
+		if(strncmp(buffer,"exit",4)==0){ 	// if the command "exit" is asked
+			write(STDOUT_FILENO, bye, strlen(bye)); 	// display goodbye message
+			exit(EXIT_SUCCESS); 	// exit the program succesfully
 		}
 	    
 	    pid_t pid;
